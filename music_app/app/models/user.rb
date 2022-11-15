afc666eb5250e235c8da38acca14a54b0697dbbd
evgenii-shvetsov <sea.dev91@gmail.com>
2022-11-15 14:21:37 -0800

    Create routes and controller

--- music_app/app/models/user.rb
+++ music_app/app/models/user.rb
@@ -10,6 +10,8 @@
 #  updated_at      :datetime         not null
 #
 class User < ApplicationRecord
+    attr_reader :password
+
     validates :email, presence: true, uniqueness: true
     validates :password_digest, presence: true
     validates :session_token, presence: true, uniqueness: true
@@ -17,10 +19,47 @@ class User < ApplicationRecord
 
     before_validates :ensure_session_token
 
-    def self.find_by_credentials(username, password)
-
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        if user && user.is_password?(password)
+            return user
+        else
+            nil
+        end
     end
+
+    def password=(password)
+        @password = password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    def is_password?(password)
+        bcrypt_object = BCrypt:Password.new(self.password_digest)
+        bcrypt_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
     
+    def generate_unique_session_token
+        token = Secure_random::urlsafe_base64
+        while User.exists?(session_token: session_token)
+            token = Secure_random::urlsafe_base64
+        end
+        token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+
+
 
 
 end
